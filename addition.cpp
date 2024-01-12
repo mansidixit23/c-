aef41adbb6b9a71ae5b34d7dede796030342ccc1
Mansi Dixit <135194705+mansidixit23@users.noreply.github.com>
2024-01-12 17:33:00 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int n1 , n2 , n3 ;
+    cout<<"enter two numbers:\n";
+    cin>>n1;
+    cin>>n2;
+    n3 = n2+n1;
+    cout<<"sum is:"<<n3;
+}
